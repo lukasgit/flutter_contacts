573097962c1b146c3451e9d38bcc8a0913ba94f8
Timur Panzhiev <timur.panzhiev@itomy.ch>
2020-09-14 13:25:28 +0300

    added androidLocalizedLabels on android side

--- android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
+++ android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
@@ -102,10 +102,10 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
   public void onMethodCall(MethodCall call, Result result) {
     switch(call.method){
       case "getContacts": {
-        this.getContacts(call.method, (String)call.argument("query"), (boolean)call.argument("withThumbnails"), (boolean)call.argument("photoHighResolution"), (boolean)call.argument("orderByGivenName"), result);
+        this.getContacts(call.method, (String)call.argument("query"), (boolean)call.argument("withThumbnails"), (boolean)call.argument("photoHighResolution"), (boolean)call.argument("orderByGivenName"), (boolean)call.argument("androidLocalizedLabels"), result);
         break;
       } case "getContactsForPhone": {
-        this.getContactsForPhone(call.method, (String)call.argument("phone"), (boolean)call.argument("withThumbnails"), (boolean)call.argument("photoHighResolution"), (boolean)call.argument("orderByGivenName"), result);
+        this.getContactsForPhone(call.method, (String)call.argument("phone"), (boolean)call.argument("withThumbnails"), (boolean)call.argument("photoHighResolution"), (boolean)call.argument("orderByGivenName"), (boolean)call.argument("androidLocalizedLabels"), result);
         break;
       } case "getAvatar": {
         final Contact contact = Contact.fromMap((HashMap)call.argument("contact"));
@@ -137,23 +137,28 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
         break;
       } case "openExistingContact" :{
         final Contact contact = Contact.fromMap((HashMap)call.argument("contact"));
+        final boolean localizedLabels = call.argument("androidLocalizedLabels");
         if (delegate != null) {
           delegate.setResult(result);
+          delegate.setLocalizedLabels(localizedLabels);
           delegate.openExistingContact(contact);
         } else {
           result.success(FORM_COULD_NOT_BE_OPEN);
         }
         break;
       } case "openContactForm": {
+        final boolean localizedLabels = call.argument("androidLocalizedLabels");
         if (delegate != null) {
           delegate.setResult(result);
+          delegate.setLocalizedLabels(localizedLabels);
           delegate.openContactForm();
         } else {
           result.success(FORM_COULD_NOT_BE_OPEN);
         }
         break;
       } case "openDeviceContactPicker": {
-        openDeviceContactPicker(result);
+        final boolean localizedLabels = call.argument("androidLocalizedLabels");
+        openDeviceContactPicker(result, localizedLabels);
         break;
       } default: {
         result.notImplemented();
@@ -199,12 +204,12 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
 
 
   @TargetApi(Build.VERSION_CODES.ECLAIR)
-  private void getContacts(String callMethod, String query, boolean withThumbnails, boolean photoHighResolution, boolean orderByGivenName, Result result) {
-    new GetContactsTask(callMethod, result, withThumbnails, photoHighResolution, orderByGivenName).executeOnExecutor(executor, query, false);
+  private void getContacts(String callMethod, String query, boolean withThumbnails, boolean photoHighResolution, boolean orderByGivenName, boolean localizedLabels, Result result) {
+    new GetContactsTask(callMethod, result, withThumbnails, photoHighResolution, orderByGivenName, localizedLabels).executeOnExecutor(executor, query, false);
   }
 
-  private void getContactsForPhone(String callMethod, String phone, boolean withThumbnails, boolean photoHighResolution, boolean orderByGivenName, Result result) {
-    new GetContactsTask(callMethod, result, withThumbnails, photoHighResolution, orderByGivenName).executeOnExecutor(executor, phone, true);
+  private void getContactsForPhone(String callMethod, String phone, boolean withThumbnails, boolean photoHighResolution, boolean orderByGivenName, boolean localizedLabels, Result result) {
+    new GetContactsTask(callMethod, result, withThumbnails, photoHighResolution, orderByGivenName, localizedLabels).executeOnExecutor(executor, phone, true);
   }
 
   @Override
@@ -240,9 +245,14 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
     private static final int REQUEST_OPEN_EXISTING_CONTACT = 52942;
     private static final int REQUEST_OPEN_CONTACT_PICKER = 52943;
     private Result result;
+    private boolean localizedLabels;
 
     void setResult(Result result) {
       this.result = result;
+    }
+
+    void setLocalizedLabels(boolean localizedLabels) {
+      this.localizedLabels = localizedLabels;
     }
 
     void finishWithResult(Object result) {
@@ -257,7 +267,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
       if(requestCode == REQUEST_OPEN_EXISTING_CONTACT || requestCode == REQUEST_OPEN_CONTACT_FORM) {
         try {
           Uri ur = intent.getData();
-          finishWithResult(getContactByIdentifier(ur.getLastPathSegment()));
+          finishWithResult(getContactByIdentifier(ur.getLastPathSegment(), localizedLabels));
         } catch (NullPointerException e) {
           finishWithResult(FORM_OPERATION_CANCELED);
         }
@@ -273,7 +283,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
         Cursor cursor = contentResolver.query(contactUri, null, null, null, null);
         if (cursor.moveToFirst()) {
           String id = contactUri.getLastPathSegment();
-          getContacts("openDeviceContactPicker", id, false, false, false, this.result);
+          getContacts("openDeviceContactPicker", id, false, false, false, localizedLabels, this.result);
         } else {
           Log.e(LOG_TAG, "onActivityResult - cursor.moveToFirst() returns false");
           finishWithResult(FORM_OPERATION_CANCELED);
@@ -289,7 +299,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
     void openExistingContact(Contact contact) {
       String identifier = contact.identifier;
       try {
-        HashMap contactMapFromDevice = getContactByIdentifier(identifier);
+        HashMap contactMapFromDevice = getContactByIdentifier(identifier, localizedLabels);
         // Contact existence check
         if(contactMapFromDevice != null) {
           Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, identifier);
@@ -323,7 +333,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
     void startIntent(Intent intent, int request) {
     }
 
-    HashMap getContactByIdentifier(String identifier) {
+    HashMap getContactByIdentifier(String identifier, boolean localizedLabels) {
       ArrayList<Contact> matchingContacts;
       {
         Cursor cursor = contentResolver.query(
@@ -333,7 +343,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
                 null
         );
         try {
-          matchingContacts = getContactsFrom(cursor);
+          matchingContacts = getContactsFrom(cursor, localizedLabels);
         } finally {
           if(cursor != null) {
             cursor.close();
@@ -347,9 +357,10 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
     }
   }
   
-    private void openDeviceContactPicker(Result result) {
+    private void openDeviceContactPicker(Result result, boolean localizedLabels) {
       if (delegate != null) {
         delegate.setResult(result);
+        delegate.setLocalizedLabels(localizedLabels);
         delegate.openContactPicker();
       } else {
         result.success(FORM_COULD_NOT_BE_OPEN);
@@ -414,22 +425,24 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
     private boolean withThumbnails;
     private boolean photoHighResolution;
     private boolean orderByGivenName;
+    private boolean localizedLabels;
 
-    public GetContactsTask(String callMethod, Result result, boolean withThumbnails, boolean photoHighResolution, boolean orderByGivenName){
+    public GetContactsTask(String callMethod, Result result, boolean withThumbnails, boolean photoHighResolution, boolean orderByGivenName, boolean localizedLabels) {
       this.callMethod = callMethod;
       this.getContactResult = result;
       this.withThumbnails = withThumbnails;
       this.photoHighResolution = photoHighResolution;
       this.orderByGivenName = orderByGivenName;
+      this.localizedLabels = localizedLabels;
     }
 
     @TargetApi(Build.VERSION_CODES.ECLAIR)
     protected ArrayList<HashMap> doInBackground(Object... params) {
       ArrayList<Contact> contacts;
       switch (callMethod) {
-        case "openDeviceContactPicker": contacts = getContactsFrom(getCursor(null, (String) params[0])); break;
-        case "getContacts": contacts = getContactsFrom(getCursor((String) params[0], null)); break;
-        case "getContactsForPhone": contacts = getContactsFrom(getCursorForPhone(((String) params[0]))); break;
+        case "openDeviceContactPicker": contacts = getContactsFrom(getCursor(null, (String) params[0]), localizedLabels); break;
+        case "getContacts": contacts = getContactsFrom(getCursor((String) params[0], null), localizedLabels); break;
+        case "getContactsForPhone": contacts = getContactsFrom(getCursorForPhone(((String) params[0])), localizedLabels); break;
         default: return null;
       }
 
@@ -529,7 +542,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
    * @param cursor
    * @return the list of contacts
    */
-  private ArrayList<Contact> getContactsFrom(Cursor cursor) {
+  private ArrayList<Contact> getContactsFrom(Cursor cursor, boolean localizedLabels) {
     HashMap<String, Contact> map = new LinkedHashMap<>();
 
     while (cursor != null && cursor.moveToNext()) {
@@ -563,7 +576,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
         String phoneNumber = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
         if (!TextUtils.isEmpty(phoneNumber)){
           int type = cursor.getInt(cursor.getColumnIndex(Phone.TYPE));
-          String label = Item.getPhoneLabel(resources, type, cursor, true);
+          String label = Item.getPhoneLabel(resources, type, cursor, localizedLabels);
           contact.phones.add(new Item(label, phoneNumber, type));
         }
       }
@@ -572,7 +585,7 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
         String email = cursor.getString(cursor.getColumnIndex(Email.ADDRESS));
         int type = cursor.getInt(cursor.getColumnIndex(Email.TYPE));
         if (!TextUtils.isEmpty(email)) {
-          String label = Item.getEmailLabel(resources, type, cursor, true);
+          String label = Item.getEmailLabel(resources, type, cursor, localizedLabels);
           contact.emails.add(new Item(label, email, type));
         }
       }

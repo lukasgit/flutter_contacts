d95cdb11de2009b09047933ca29e6f44e6903f74
Manh Duc NGUYEN <mdnguyen@linagora.com>
2021-03-23 14:43:22 +0700

    Add getContactsByEmailOrName() for contacts_service and implement for iOS plugin

--- lib/contacts_service.dart
+++ lib/contacts_service.dart
@@ -81,15 +81,13 @@ class ContactsService {
       {bool withThumbnails = true,
         bool photoHighResolution = true,
         bool orderByGivenName = true,
-        bool iOSLocalizedLabels = true,
-        bool androidLocalizedLabels = true}) async {
+        bool iOSLocalizedLabels = true}) async {
     Iterable contacts = await _channel.invokeMethod('getContactsByEmailOrName',<String,dynamic>{
       'query': query,
       'withThumbnails': withThumbnails,
       'photoHighResolution': photoHighResolution,
       'orderByGivenName': orderByGivenName,
       'iOSLocalizedLabels': iOSLocalizedLabels,
-      'androidLocalizedLabels': androidLocalizedLabels,
     });
     return contacts.map((m) => Contact.fromMap(m));
   }

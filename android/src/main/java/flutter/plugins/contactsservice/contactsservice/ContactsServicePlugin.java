2c32443f60f474303542bf02ce51b26c65ea4229
Lukebot19 <103214604+Lukebot19@users.noreply.github.com>
2024-02-01 20:27:33 +0000

    Update ContactsServicePlugin.java

--- android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
+++ android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
@@ -296,7 +296,9 @@ public class ContactsServicePlugin implements MethodCallHandler, FlutterPlugin, 
           Log.e(LOG_TAG, "onActivityResult - cursor.moveToFirst() returns false");
           finishWithResult(FORM_OPERATION_CANCELED);
         }}else{return true;}
-        cursor.close();
+        if (cursor != null) {
+        cursor.close();          
+        }
         return true;
       }
 
